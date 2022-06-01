45036abd43c550d9fd587f29c6032b4bb94b2201
miguez1906 <48599141+miguez1906@users.noreply.github.com>
2022-06-01 11:17:47 -0700

    Update IndexTest
    comments

--- src/application/Test/IndexTest.java
+++ src/application/Test/IndexTest.java
@@ -5,6 +5,9 @@ import org.junit.Test;
 public class IndexTest {
     @Test
     public void testKanyeCorpus(){
+        //inverted index by hand
+        //run test corpus through your index
+        //assertequal index by hand by
 
     }
 }

5decff4c1da9a2769b464a20eea4df3c48ad3b08
caitlinmart <caitlinmart@users.noreply.github.com>
2022-06-05 17:06:56 -0700

    search.java file for user's searches

---
+++ src/application/UI/Search.java
@@ -0,0 +1,21 @@
+package application.UI;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Search {
+
+    //Initialization of the frame, button, label, and panel for the UI
+
+    JButton submitResult = new JButton("Submit");
+    JLabel title = new JLabel();
+    JPanel panel = new JPanel();
+
+    public void Search(){
+
+    }
+
+
+
+
+}

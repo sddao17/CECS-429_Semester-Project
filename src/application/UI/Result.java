52c044a409d04bd0d52368c82314b2cbb2a0e422
caitlinmart <caitlinmart@users.noreply.github.com>
2022-06-05 22:12:17 -0700

    creation of results class for the UI

---
+++ src/application/UI/Result.java
@@ -0,0 +1,20 @@
+package application.UI;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Result {
+
+
+    public Component resultsUI(){
+        JPanel panel = new JPanel();
+
+
+        return panel;
+    }
+
+    public Component displayContent(){
+       JPanel content = new JPanel();
+       return content;
+    }
+}
